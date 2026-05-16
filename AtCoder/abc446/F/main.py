99b6b87774e1998a47c21dcbe7e927e85e130234
ryoga-miyaguni <rigongguo@gmail.com>
2026-05-16 15:39:36 +0900

    abc446 : A

---
+++ AtCoder/abc446/F/main.py
@@ -0,0 +1,36 @@
+import sys
+from math import gcd, ceil, floor, sqrt, pi, factorial
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+
+# 入力高速化
+input = sys.stdin.readline
+
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input().rstrip() # 改行文字削除
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+# 再帰上限の設定（DFS用）
+sys.setrecursionlimit(10 ** 6)
+
+# 定数
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    
+    pass
+
+if __name__ == '__main__':
+    main()
